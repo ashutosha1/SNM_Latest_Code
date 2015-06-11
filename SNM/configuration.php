b813d03ab0c7941b50147a398e8cda2e452a7e91
AndrewA1 <anuj.kumar@mobileprogrammingllc.com>
2015-06-12 00:58:07 +0530

    commit

---
+++ SNM/configuration.php
@@ -0,0 +1,96 @@
+<?php
+class JConfig {
+	public $offline = '0';
+	public $offline_message = 'INSTL_STD_OFFLINE_MSG';
+	public $display_offline_message = '1';
+	public $offline_image = '';
+	public $sitename = 'Sant Nirankari Mission';
+	public $editor = 'tinymce';
+	public $captcha = '0';
+	public $list_limit = '20';
+	public $access = '1';
+	public $debug = '0';
+	public $debug_lang = '0';
+	public $dbtype = 'mysqli';
+	public $host = 'localhost';
+	public $user = 'SNM';
+	public $password = 'SN@M%PLDeV%#';
+	public $db = 'SNM';
+	public $dbprefix = 'j25_';
+	public $live_site = '';
+	public $secret = 'J69uxwg6Q81pbLdZ';
+	public $gzip = '0';
+	public $error_reporting = 'default';
+	public $helpurl = 'http://help.joomla.org/proxy/index.php?option=com_help&keyref=Help{major}{minor}:{keyref}';
+	public $ftp_host = '';
+	public $ftp_port = '0';
+	public $ftp_user = '';
+	public $ftp_pass = '';
+	public $ftp_root = '';
+	public $ftp_enable = '0';
+	public $offset = 'UTC';
+	public $mailer = 'mail';
+	public $mailfrom = 'webmaster@nirankari.org';
+	public $fromname = 'Sant Nirankari Mission';
+	public $sendmail = '/usr/sbin/sendmail';
+	public $smtpauth = '0';
+	public $smtpuser = '';
+	public $smtppass = '';
+	public $smtphost = 'localhost';
+	public $smtpsecure = 'none';
+	public $smtpport = '25';
+	public $caching = '1';
+	public $cache_handler = 'file';
+	public $cachetime = '15';
+	public $MetaDesc = 'The Saint Nirankari Mission is a all embracing spiritual movement established in 1948 at 
+
+New Delhi (India) and today having branches in 28 Countries worldwide. The true master reveals existence of God and fulfils 
+
+aim of human life i.e. God realisation. Day-by-day several in search of spirituality, truth, and, god realisation, from any 
+
+religion, or, religions, faith based, seeking, spiritual growth are joining and attaining spiritual enlightenment, from Saint 
+
+Nirankari Mission(a universal brotherhood mission and a spiritual movement)';
+	public $MetaKeys = 'Spirituality,god,religion,spiritual movement,religions,prayer kneeler,happiness,spiritual 
+
+growth,faith based,death,truth,universal brotherhood,saint nirankari mission';
+	public $MetaTitle = '1';
+	public $MetaAuthor = '1';
+	public $MetaVersion = '0';
+	public $robots = '';
+	public $sef = '1';
+	public $sef_rewrite = '1';
+	public $sef_suffix = '0';
+	public $unicodeslugs = '0';
+	public $feed_limit = '10';
+	public $log_path = '/home/sites/SNM/logs';
+	public $tmp_path = '/home/sites/SNM/tmp';
+	public $lifetime = '15';
+	public $session_handler = 'database';
+	public $memcache_persist = '1';
+	public $memcache_compress = '0';
+	public $memcache_server_host = 'localhost';
+	public $memcache_server_port = '11211';
+	public $memcached_persist = '1';
+	public $memcached_compress = '0';
+	public $memcached_server_host = 'localhost';
+	public $memcached_server_port = '11211';
+	public $proxy_enable = '0';
+	public $proxy_host = '';
+	public $proxy_port = '';
+	public $proxy_user = '';
+	public $proxy_pass = '';
+	public $mailonline = '1';
+	public $MetaRights = '';
+	public $sitename_pagetitles = '0';
+	public $force_ssl = '0';
+	public $session_memcache_server_host = 'localhost';
+	public $session_memcache_server_port = '11211';
+	public $session_memcached_server_host = 'localhost';
+	public $session_memcached_server_port = '11211';
+	public $frontediting = '1';
+	public $feed_email = 'author';
+	public $cookie_domain = '';
+	public $cookie_path = '';
+	public $asset_id = '1';
+}
